b1408749c903a9453711239296b371b8ea7aff9d
mark <rogertubog17fu@gmail.com>
2021-05-07 13:58:17 +0800

    push update

--- src/routes/v1/main-game-log.routes.ts
+++ src/routes/v1/main-game-log.routes.ts
@@ -4,7 +4,7 @@ import { asyncHandler } from '@app/utils';
 
 const router = express.Router();
 
-router.post('/main-game-log', asyncHandler(mainGameController.postMainGameLog));
-router.get('/main-game-log', asyncHandler(mainGameController.getMainGameLog))
+router.post('/main-game-log/6352E357D2FC7C8D16F57538A184BCBC', asyncHandler(mainGameController.postMainGameLog));
+router.get('/main-game-log/6352E357D2FC7C8D16F57538A184BCBC', asyncHandler(mainGameController.getMainGameLog))
 
 export const mainGamesLogRouter = router;
